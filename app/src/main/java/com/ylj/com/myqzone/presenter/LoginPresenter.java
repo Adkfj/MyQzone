24d028d55e8320e4f71a8a21444b4489967198c3
Adkfj <1378398894@qq.com>
2017-12-20 23:56:06 +0800

    修改

--- app/src/main/java/com/ylj/com/myqzone/presenter/LoginPresenter.java
+++ app/src/main/java/com/ylj/com/myqzone/presenter/LoginPresenter.java
@@ -4,6 +4,12 @@ import com.ylj.com.myqzone.model.api.ApiService;
 
 import javax.inject.Inject;
 
+import okhttp3.ResponseBody;
+import retrofit2.Response;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+
 /**
  * Created by pc on 2017/12/6.
  */
@@ -20,6 +26,21 @@ public class LoginPresenter implements LoginContrct.Presenter {
 
     @Override
     public void login(String user, String password) {
+        apiService.login(user,password).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread()).subscribe(new Subscriber<ResponseBody>() {
+            @Override
+            public void onCompleted() {
 
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onNext(ResponseBody responseBody) {
+
+            }
+        });
     }
 }
